d9ee5dfbbb73839801ef5e4785302a40648e2089
2403_89608231 <zzc1529119384@163.com>
2025-11-25 01:13:39 +0800

    多文件上传Mock测试成功

--- src/views/temp1.vue
+++ src/views/temp1.vue
@@ -11,19 +11,57 @@
 
 
 
-    <el-upload class="upload-demo" drag action="https://run.mocky.io/v3/9d059bf9-4660-45f2-925d-ce80ad6c4d15" multiple>
-      <el-icon class="el-icon--upload"><upload-filled /></el-icon>
-      <div class="el-upload__text">
-        Drop file here or <em>click to upload</em>
-      </div>
-      <template #tip>
-        <div class="el-upload__tip">
-          jpg/png files with a size less than 500kb
+
+
+
+
+
+
+
+    <el-button @click="fileUploadDialog = true">文件上传</el-button>
+    <el-button>文件夹上传</el-button>
+
+
+
+
+
+
+
+
+
+
+
+
+
+    <el-dialog v-model="fileUploadDialog" title="文件上传" width="500" modal :before-close="handleClose"
+      :close-on-click-modal="false" :close-on-press-escape="false" :show-close="false">
+
+
+      <el-upload drag action="http://127.0.0.1:4523/m1/7374019-7106024-default/upload" :auto-upload="false" multiple
+        ref="uploadFiles" :on-success="fileUploadSuccess">
+        <el-icon class="el-icon--upload"><upload-filled /></el-icon>
+        <div class="el-upload__text">
+          将文件拖到此处或<em>点击上传</em>
+        </div>
+        <template #tip>
+          <div>
+            温馨提示温馨提示温馨提示温馨提示
+          </div>
+        </template>
+      </el-upload>
+
+
+
+
+      <template #footer>
+        <div class="dialog-footer">
+          <el-button @click="fileUploadDialog = false">取消</el-button>
+          <el-button type="primary" @click="fileUpload">
+            上传
+          </el-button>
         </div>
       </template>
-    </el-upload>
-
-
+    </el-dialog>
 
 
 
@@ -43,6 +81,21 @@
 <script setup>
 
 import { UploadFilled } from '@element-plus/icons-vue'
+import { ref } from 'vue';
+const fileUploadDialog = ref(false)
+const uploadFiles = ref(null)
+const fileUpload = () => {
+  uploadFiles.value.submit()
+}
+const fileUploadSuccess = (response, file, fileList)=>{
+  console.log(response);
+  // console.log(file);
+  // console.log(fileList);
+  fileUploadSuccessMsg();
+}
+
+
+
 
 </script>
 
