4eb8e0a9f73e755128ce8fa186a2af1b1b1f7060
2403_89608231 <zzc1529119384@163.com>
2025-11-25 22:49:03 +0800

    处理axios的封装问题，请求拦截器，以及请求带上token

--- src/utils/request.js
+++ src/utils/request.js
@@ -1,13 +1,31 @@
 import axios from "axios";
 
 const request = axios.create({
-  baseURL: "",
+  baseURL: "http://localhost:8080",
   timeout: 5000
 });
 
+// 请求拦截器
 request.interceptors.request.use(
-  (request) => {
-    return request.data;
+  (config) => {
+    const loginUser = JSON.parse(localStorage.getItem("loginUser"))
+    console.log(loginUser);
+
+    if (loginUser && loginUser.token) {
+      config.headers.token = loginUser.token;
+    }
+    console.log("最终发送的 headers =", config.headers)
+    return config;   // ✔ 必须返回 config
+  },
+  (error) => {
+    return Promise.reject(error);
+  }
+);
+
+// 响应拦截器
+request.interceptors.response.use(
+  (response) => {
+    return response.data;  // ✔ 这里才返回 data
   },
   (error) => {
     return Promise.reject(error);
